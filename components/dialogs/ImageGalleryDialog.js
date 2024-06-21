d75479f7c989ba8be9169a6c64d2bea02525f57b
Gui Prá <gui@guiprav.com>
2024-06-21 12:14:22 -0300

    .

--- components/dialogs/ImageGalleryDialog.js
+++ components/dialogs/ImageGalleryDialog.js
@@ -51,11 +51,15 @@ class ImageGalleryDialog {
           <select class="outline-none bg-transparent *:bg-[#121212] *:text-neutral-100">
             ${d.usePlaceholderTag('option', d.map(() => this.folders, x => d.html`<option ${{ value: x, selected: () => this.selectedFolder === x }}>${x}</option>`))}
           </select>
-          <div>
+          <div class="flex">
             ${d.if(() => this.images.length, d.html`<button type="button" class="nf nf-fa-cloud_upload text-2xl hover:bg-black/70 outline-none rounded-b px-3 pt-1 pb-2" ${{
               onClick: this.upload,
             }}>`)}
-            <button type="button" class="nf text-2xl hover:bg-black/70 nf-md-robot_happy outline-none rounded-b px-3 pt-1 pb-2" ${{ onClick: this.ai }}></button>
+            <button type="button" class="text-2xl hover:bg-black/70 outline-none rounded-b px-3 pt-1 pb-2" ${{ onClick: this.ai }}>
+              <svg viewBox="0 0 24 24" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill="currentColor" d="m9 4l2.5 5.5L17 12l-5.5 2.5L9 20l-2.5-5.5L1 12l5.5-2.5zm0 4.83L8 11l-2.17 1L8 13l1 2.17L10 13l2.17-1L10 11zM19 9l-1.26-2.74L15 5l2.74-1.25L19 1l1.25 2.75L23 5l-2.75 1.26zm0 14l-1.26-2.74L15 19l2.74-1.25L19 15l1.25 2.75L23 19l-2.75 1.26z"/>
+              </svg>
+            </button>
           </div>
         </div>
         ${d.if(() => !this.images.length, d.html`
