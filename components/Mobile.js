0410b59ed7934909f5eabda116e3bfd848ffba5e
Gui Prá <gui@guiprav.com>
2024-07-02 09:18:20 -0300

    .

--- components/Mobile.js
+++ components/Mobile.js
@@ -1,22 +1,23 @@
 class Mobile {
   render = () => d.html`
-      <div class="h-[50vh] bg-[#121212] text-[#faf9f6] p-6 flex flex-col">
-        <div class="gfont-[Pacifico] text-4xl text-center mt-12">Webfoundry</div>
-      </div>
-      <div class="flow-root h-[50vh] p-3 bg-[#faf9f6]">
-        <div class="-mt-[25vh]">
-          <div class="text-center whitespace-pre text-[#faf9f6] mb-8">The precision visual
+    <div class="h-[50vh] bg-[#121212] text-[#faf9f6] p-6 flex flex-col">
+      <div class="gfont-[Pacifico] text-4xl text-center mt-12">Webfoundry</div>
+    </div>
+    <div class="flow-root h-[50vh] bg-[#faf9f6] text-[#121212]">
+      <div class="-mt-[25vh]">
+        <div class="text-center whitespace-pre text-[#faf9f6] mb-8 mx-3">The precision visual
 Tailwind CSS editor</div>
-          <div class="max-w-xl mx-auto">
-            <div style="padding:56.25% 0 0 0;position:relative;">
-              <iframe src="https://player.vimeo.com/video/854399081?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Get Webfoundry" data-ready="true"></iframe>
-            </div>
-            <script src="https://player.vimeo.com/api/player.js"></script>
+        <div class="max-w-xl mx-auto">
+          <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/854399081?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Get Webfoundry" data-ready="true"></iframe>
           </div>
-          <div class="text-center whitespace-pre mt-8 text-[#121212]">Open this site on a desktop browser to try it out!
-No account creation necessary!</div>
+          <script src="https://player.vimeo.com/api/player.js"></script>
         </div>
+        <div class="text-center mt-8 text-[#121212] whitespace-pre-wrap mx-3">Open this site on a desktop
+browser to try it out!
+No account creation necessary!</div>
       </div>
+    </div>
   `;
 }
 
